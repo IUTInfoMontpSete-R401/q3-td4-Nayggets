7b5a5f65e4e3c6644cdcae99f4ace2c1c0338cc8
Quentin_Melotte <quentin.melotte@etu.umontpellier.fr>
2023-04-07 11:41:58 +0200

    SudokuObserver

--- src/main/java/pattern/Command/SetValueCommand.java
+++ src/main/java/pattern/Command/SetValueCommand.java
@@ -1,14 +1,33 @@
 package pattern.Command;
 
+import pattern.Model.SudokuModel;
+
 public class SetValueCommand implements SudokuCommand{
+
+    private int col;
+    private int row;
+    private SudokuModel sudokuModel;
+    private int oldValue;
+    private int newValue;
 
     @Override
     public void execute() {
-
+        oldValue = sudokuModel.getValueAt(row,col);
+        sudokuModel.setValueAt(row,col,newValue);
     }
 
     @Override
     public void undo() {
+        sudokuModel.setValueAt(row,col,oldValue);
+    }
 
+
+    public SetValueCommand(int col, int row, SudokuModel sudokuModel, int newValue) {
+        this.col = col;
+        this.row = row;
+        this.sudokuModel = sudokuModel;
+        this.oldValue = oldValue;
+        this.newValue = newValue;
     }
 }
+
