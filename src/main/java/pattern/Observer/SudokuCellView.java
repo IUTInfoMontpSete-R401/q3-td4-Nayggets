b34f278514e664fd37b80008cc657e3ce9378807
Quentin_Melotte <quentin.melotte@etu.umontpellier.fr>
2023-04-07 10:57:11 +0200

    SudokuObserver

--- src/main/java/pattern/Observer/SudokuCellView.java
+++ src/main/java/pattern/Observer/SudokuCellView.java
@@ -1,32 +1,25 @@
-package pattern.Observer;
+package Pattern.Observers;
 
+import Pattern.Composite.Composite;
+import Pattern.View.SudokuView;
 import pattern.Observer.SudokuObserver;
 
 public class SudokuCellView implements SudokuObserver {
-    @Override
-    public void update(int[][] board) {
-            System.out.println("Cell at row " + row + ", column " + col + " updated to " + value);
-            display();
+    private Composite cell;
+    private int row;
+    private int col;
+
+    public SudokuCellView(Composite cell, int row, int col) {
+        this.cell = cell;
+        this.row=row;
+        this.col=col;
     }
 
-    public void display() {
-        for (int row = 0; row < this.getBoardSize(); row++) {
-            if (row % this.getBlockSize() == 0) {
-                System.out.println(" -----------------------");
-            }
-            for (int col = 0; col < this.getBoardSize(); col++) {
-                if (col % this.getBlockSize() == 0) {
-                    System.out.print("| ");
-                }
-                int value = this.getValueAt(row, col);
-                if (value == 0) {
-                    System.out.print("  ");
-                } else {
-                    System.out.print(value + " ");
-                }
-            }
-            System.out.println("|");
+    @Override
+    public void update(int row, int col, int value) {
+        if(row==this.row && col==this.col){
+            System.out.println("Cell at row " + row + ", column " + col + " updated to " + value);
+            cell.setValue(value);
         }
-        System.out.println(" -----------------------");
     }
 }
